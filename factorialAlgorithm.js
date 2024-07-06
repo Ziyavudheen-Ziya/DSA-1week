a9ef49d7fbe64698a4f486661611ef78c2d69b5b
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-06 12:50:30 +0530

    factorila added

--- factorialAlgorithm.js
+++ factorialAlgorithm.js
@@ -12,7 +12,11 @@ function factorial(n){
    
       
 }
+console.log(factorial(0));
+console.log(factorial(1));
 console.log(factorial(3));
 console.log(factorial(4));
 console.log(factorial(5));
 console.log(factorial(6));
+
+//Big-O = O(n)
