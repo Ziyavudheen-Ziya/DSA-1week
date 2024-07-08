15a20e34122d320de696253d06321e66ac3382ee
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-08 18:32:05 +0530

    recursiveBinarySearch

---
+++ recursiveBinarySearchAlgorithm.js
@@ -0,0 +1,20 @@
+function recuresiveBinarySearchAlgoritm(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
+}
+
+function search(arr, target, left, right) {
+  let mid = Math.floor((left + right) / 2);
+  if (left > right) {
+    return -1;
+  }
+
+  if (arr[mid] == target) {
+    return mid;
+  } else if (arr[mid] < target) {
+    return search(arr, target, left + 1, right);
+  } else {
+    return search(arr, target, left, right - 1);
+  }
+}
+
+console.log(recuresiveBinarySearchAlgoritm([1, 2, 3, 4, 5, 6, 7, 8], 5));
