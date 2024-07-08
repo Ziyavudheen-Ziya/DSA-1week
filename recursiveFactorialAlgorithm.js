b34dbd3c6af2cddee577ead8efb06795aacc6311
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-08 15:22:47 +0530

    recursive Factorial

---
+++ recursiveFactorialAlgorithm.js
@@ -0,0 +1,11 @@
+function recursivefactorial(n) {
+  if (n == 0) {
+    return 1;
+  }
+
+  return n * recursivefactorial(n - 1);
+}
+
+console.log(recursivefactorial(4));
+console.log(recursivefactorial(5));
+console.log(recursivefactorial(6));
