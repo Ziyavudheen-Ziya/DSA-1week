4b914c4e87d24e4b5d52453ef2ee02aab10b4473
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-09 11:22:37 +0530

    recursionLargest

--- recursionLargestValue.js
+++ recursionLargestValue.js
@@ -1,6 +1,6 @@
 function recursionLargest(arr, l) {
   if (arr.length == 0) {
-    return sl;
+    return l;
   }
 
   if (arr[0] > l) {
@@ -14,3 +14,33 @@ console.log(recursionLargest([3, 4, 5, 2, 1], 0));
 
 
 // Big-O = O(n) -linear.
+
+
+//SECOND LARGEST
+
+function recursionSecondLargest(arr,l,sl){
+   
+
+    if(arr.length==0){
+
+        return sl
+    }
+
+  
+
+    if(arr[0]>l){
+        sl=l
+        l=arr[0]
+    }else if(arr[0]>sl&&sl<l){
+
+        sl=arr[0]
+    }
+
+    return recursionSecondLargest(arr.slice(1),l,sl)
+
+
+
+
+}
+
+console.log(recursionSecondLargest([3,4,7,1,18,30],0));
