730257c96447ac2a22a4bbfc48ae79f86e60b76f
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-09 14:22:08 +0530

    recursionReverse

--- recursionReverseString.js
+++ recursionReverseString.js
@@ -1,15 +1,11 @@
-function recursionReverse(val,str){
+function recursionReverse(val, str) {
+  if (val.length == 0) {
+    return str;
+  }
 
-    if(val.length==0){
+  str += val[val.length - 1];
 
-        return str;
-    }
-
-    str += val[val.length-1]
-
-    return recursionReverse(val.slice(0,-1),str)
-
+  return recursionReverse(val.slice(0, -1), str);
 }
 
-console.log(recursionReverse("abcde",""));
-
+console.log(recursionReverse("abcde", ""));
